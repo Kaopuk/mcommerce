eaeb1a3cb6fac09346c996f6d28136d27234e971
Kaopuk <kao_1@hotmail.com>
2018-08-30 16:37:09 +0200

    rendez vos Microservices découvrable grâce à Eureka

--- microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
+++ microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
@@ -2,8 +2,14 @@ package com.mcommandes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.Mcommandes")
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class McommandesApplication {
 
 	public static void main(String[] args) {
